1842f52d257344325f19aa7e1861581bf5acec74
Edgar Zapeka <edgar.zapeka@gmail.com>
2020-03-21 18:39:03 -0700

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -49,6 +49,20 @@ impl Universe {
 
         count
     }
+
+    // get the dead and alive vals of the entire universe
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    // set cells to be alive in a universe by passing the row and column
+    // of each cell as an array
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
 }
 
 #[wasm_bindgen]
@@ -115,8 +129,18 @@ impl Universe {
         self.width
     }
 
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
     pub fn height(&self) -> u32 {
         self.height
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
     }
 
     pub fn cells(&self) -> *const Cell {
