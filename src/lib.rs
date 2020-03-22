6ba20156c29e6c7290ae43be093b8382bdc2e1ce
Edgar Zapeka <edgar.zapeka@gmail.com>
2020-03-22 10:17:08 -0700

    add manual cell modification

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,15 @@ pub enum Cell {
     Alive = 1,
 }
 
+impl Cell {
+    fn toggle(&mut self) {
+        *self = match *self {
+            Cell::Dead => Cell::Alive,
+            Cell::Alive => Cell::Dead,
+        };
+    }
+}
+
 #[wasm_bindgen]
 pub struct Universe {
     width: u32,
@@ -167,6 +176,12 @@ impl Universe {
 
     pub fn cells(&self) -> *const Cell {
         self.cells.as_ptr()
+    }
+
+    pub fn toggle_cell(&mut self, row: u32, column: u32) {
+        log!("calling toggle_cell");
+        let idx = self.get_index(row, column);
+        self.cells[idx].toggle();
     }
 }
 
